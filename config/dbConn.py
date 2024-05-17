5c62ab9b0153842441b7f57a511e8a9865150ecf
Ashani-Sansala <ashani.sansala16@gmail.com>
2024-05-17 11:09:00 +0530

    db config

--- config/dbConn.py
+++ config/dbConn.py
@@ -1,5 +1,5 @@
 # Database configuration
-HOST = 'your_db_host'
-USER = 'your_db_user'
-PASSWORD = 'your_db_password'
-DATABASE = 'your_db_name'
+HOST='localhost'
+USER='root'
+PASSWORD='ashani16'
+DATABASE='meditrack'
