b07514b4062ece2fd952d9ee1fd0de1084db671e
Ashani-Sansala <ashani.sansala16@gmail.com>
2024-07-10 11:33:20 +0530

    Add Stored Procedures for Data Manipulation
    - Add auth_procedures to handle user login
    - Add dashboard_procedures to handle Dashboard data and search functionality
    - Add managecamera_procedures to handle camera data, add new camera and update camera data
    - Add manageuser_procedures to handle user data, add new user and remove user functions
    - Add profile_procedures to handle retrieving user profile data, update user profile and reset password

---
+++ scripts/db/stored_procedures/manageuser_procedures.sql
@@ -0,0 +1,86 @@
+-- For Manage Users
+-- Procedure to get user data
+DELIMITER //
+CREATE PROCEDURE GetUserData(
+    IN search_term VARCHAR(1000)
+)
+BEGIN
+    SET @query = CONCAT("
+        SELECT 
+            u.username,
+            u.fullName,
+            DATE_FORMAT(u.birthday, '%Y-%m-%d') AS birthday,
+            u.email,
+            u.phoneNo,
+            p.positionName,
+            p.pId
+        FROM 
+            user u
+        JOIN 
+            position p ON u.pId = p.pId
+        WHERE 
+            u.username LIKE ? OR 
+            u.fullName LIKE ? OR 
+            u.birthday LIKE ? OR 
+            u.email LIKE ? OR 
+            u.phoneNo LIKE ? OR 
+            p.positionName LIKE ?
+    ");
+    SET @search_pattern = CONCAT('%', search_term, '%');
+    PREPARE stmt FROM @query;
+    EXECUTE stmt USING @search_pattern, @search_pattern, @search_pattern, 
+                      @search_pattern, @search_pattern, @search_pattern;
+    DEALLOCATE PREPARE stmt;
+END //
+DELIMITER ;
+-- -----------------------
+
+-- Procedure to remove user
+DELIMITER //
+CREATE PROCEDURE RemoveUser(
+    IN p_username CHAR(50)
+)
+BEGIN
+    DELETE FROM user WHERE username = p_username;
+END //
+DELIMITER ;
+-- -----------------------
+
+-- Procedure to add a user
+DELIMITER //
+CREATE PROCEDURE AddUser(
+    IN p_username CHAR(50),
+    IN p_password CHAR(60),
+    IN p_fullName VARCHAR(250),
+    IN p_birthday DATE,
+    IN p_email VARCHAR(100),
+    IN p_phoneNo VARCHAR(15),
+    IN p_pId CHAR(5)
+)
+BEGIN
+    INSERT INTO user (username, password, fullName, birthday, email, phoneNo, pId)
+    VALUES (p_username, p_password, p_fullName, p_birthday, p_email, p_phoneNo, p_pId);
+END //
+DELIMITER ;
+-- -----------------------
+
+-- Procedure to get positions
+DELIMITER //
+CREATE PROCEDURE GetPositions()
+BEGIN
+    SELECT pId, positionName FROM position;
+END //
+DELIMITER ;
+-- -----------------------
+
+-- Procedure to check username
+DELIMITER //
+CREATE PROCEDURE CheckUsername(
+    IN p_username CHAR(50)
+)
+BEGIN
+    SELECT COUNT(*) AS count FROM user WHERE username = p_username;
+END //
+DELIMITER ;
+-- --------------------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------------------
---
+++ scripts/db/stored_procedures/profile_procedures.sql
@@ -0,0 +1,63 @@
+-- For Profile
+-- Procedure to get user profile
+DELIMITER //
+CREATE PROCEDURE GetUserProfile(
+    IN p_username CHAR(50)
+)
+BEGIN
+    SELECT u.username, u.fullName, 
+    DATE_FORMAT(u.birthday, '%Y-%m-%d') AS birthday,
+    u.email, u.phoneNo, p.positionName, u.avatarUrl
+    FROM user u
+    JOIN position p ON u.pId = p.pId
+    WHERE u.username = p_username;
+END //
+DELIMITER ;
+-- -----------------------
+
+-- Procedure to update user profile
+DELIMITER //
+CREATE PROCEDURE UpdateUserProfile(
+    IN p_username CHAR(50),
+    IN p_fullName VARCHAR(250),
+    IN p_birthday DATE,
+    IN p_email VARCHAR(100),
+    IN p_phoneNo VARCHAR(15),
+    IN p_positionName VARCHAR(100),
+    IN p_avatarUrl CHAR(255)
+)
+BEGIN
+    DECLARE v_pId CHAR(5);
+    
+    SELECT pId INTO v_pId FROM position WHERE positionName = p_positionName;
+    
+    IF v_pId IS NOT NULL THEN
+        UPDATE user
+        SET fullName = p_fullName, 
+            birthday = p_birthday, 
+            email = p_email, 
+            phoneNo = p_phoneNo, 
+            pId = v_pId,
+            avatarUrl = COALESCE(p_avatarUrl, avatarUrl)
+        WHERE username = p_username;
+        
+        SELECT fullName, avatarUrl FROM user WHERE username = p_username;
+    ELSE
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Invalid position';
+    END IF;
+END //
+DELIMITER ;
+-- -----------------------
+
+-- Procedure to reset password
+DELIMITER //
+CREATE PROCEDURE ResetPassword(
+    IN p_username CHAR(50),
+    IN p_new_password CHAR(60)
+)
+BEGIN
+    UPDATE User SET password = p_new_password WHERE username = p_username;
+END //
+DELIMITER ;
+-- --------------------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------------------
